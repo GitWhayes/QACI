aca0f00b1a9d4cda48b9d7ec96ddb2537e5ed1b4
Wanda Hayes <nielsen.wh@hotmail.com>
2017-11-15 16:37:23 -0500

    first commit for QA Phase I

---
+++ src/test/java/aidQACIPhaseI/phaseITest.java
@@ -0,0 +1,15 @@
+package aidQACIPhaseI;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class phaseITest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		System.out.println("Test Phase I");
+	}
+
+}
